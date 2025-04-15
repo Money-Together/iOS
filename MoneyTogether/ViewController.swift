c330bd0526e0858632b88cf43dea84dc35023b57
heeoh <hio4662@naver.com>
2025-04-15 17:23:36 +0900

    Init

---
+++ MoneyTogether/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoneyTogether
+//
+//  Created by Heeoh Son on 4/15/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MoneyTogetherTests/MoneyTogetherTests.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyTogetherTests.swift
+//  MoneyTogetherTests
+//
+//  Created by Heeoh Son on 4/15/25.
+//
+
+import Testing
+@testable import MoneyTogether
+
+struct MoneyTogetherTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
